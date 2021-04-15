32445baf7a5f563aa3ead0a862588dd1e5a24f08
Gleb Bashkatov <glebbash@gmail.com>
2021-04-15 14:13:24 +0300

    fix: AsyncSettings types are now correct

--- src/utils/settings-provider.ts
+++ src/utils/settings-provider.ts
@@ -1,4 +1,6 @@
 import type { Provider, Type } from '@nestjs/common';
 
+type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never;
+
 export type SettingsProvider<T> = Exclude<Provider<T>, Type>;
-export type AsyncSettings<T> = Omit<SettingsProvider<T>, 'provide'>;
+export type AsyncSettings<T> = DistributiveOmit<SettingsProvider<T>, 'provide'>;
