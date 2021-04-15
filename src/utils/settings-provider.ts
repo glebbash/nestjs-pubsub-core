5c8422c166298d2ceeb078835b3013065817b2a2
Gleb Bashkatov <glebbash@gmail.com>
2021-04-15 14:14:24 +0300

    chore: ignore any usage

--- src/utils/settings-provider.ts
+++ src/utils/settings-provider.ts
@@ -1,5 +1,6 @@
 import type { Provider, Type } from '@nestjs/common';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never;
 
 export type SettingsProvider<T> = Exclude<Provider<T>, Type>;
