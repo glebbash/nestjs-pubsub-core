ee3d9d763017c3be0a638f3e01f363d03f978ece
Gleb Bashkatov <glebbash@gmail.com>
2021-04-15 14:02:17 +0300

    fix: export PubSubTimeoutError Move .d.ts files next to generated .js files

--- src/nestjs-pubsub-core.ts
+++ src/nestjs-pubsub-core.ts
@@ -4,5 +4,6 @@ export { PubSubModule } from './pubsub/pubsub.module';
 export { PubSubService } from './pubsub/pubsub.service';
 export { PubSubPublisherModule } from './pubsub-publisher/pubsub-publisher.module';
 export { PubSubPublisherService } from './pubsub-publisher/pubsub-publisher.service';
+export { PubSubTimeoutError } from './pubsub-publisher/pubsub-publisher.service';
 export { InjectTopic, getTopicToken } from './utils/topic-token';
 export { InjectSubscription, getSubscriptionToken } from './utils/subscription-token';
